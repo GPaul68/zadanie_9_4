aad5ab1299e1730c7c3f33498f0066aaad52c0ca
Paweł Galeja <pgaleja@gmail.com>
2018-01-02 18:35:40 +0100

    Add files and directores

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//scripts.js
+
+var a = prompt('Enter base a');
+var h = prompt('Enter height h');
+var triangleArea = a * h / 2;
+alert('Triangle field: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
